18a9da6ddf9252a1a71144ba30de4fa2a492b321
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-05 15:26:53 +0300

    tasks 1 -> 6

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function 'floor' that takes a float 'n' as argument
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float
+    """
+    return int(n)
